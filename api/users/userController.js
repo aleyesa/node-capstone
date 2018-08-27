bd12ffb04802fb4f52ecbb99ba72ded6f72b393d
unknown <aaron.leyesa@gmail.com>
2018-08-27 15:08:01 -0700

    implemented post request

--- api/users/userController.js
+++ api/users/userController.js
@@ -17,6 +17,7 @@ const getAllUsers = (req, res) => {
   .then(user => {
     res.json({users: user.map(accountInfo => {
       return {
+        _id: accountInfo._id,
         firstName: accountInfo.firstName,
         lastName: accountInfo.lastName,
         username: accountInfo.username,
@@ -24,27 +25,53 @@ const getAllUsers = (req, res) => {
       }
     })})
   })
-  .catch(err => console.error(err));
+  .catch(err => console.log(err));
 };
 
-const getUserById = (req, res) => {
-
+const getUser = (req, res) => {
+  User.findById(req.params.id)
+  .then(user => res.json({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    username: user.username,
+    email: user.email
+  }))
 };
 
 const createUser = (req, res) => {
-  return User.create({
+  //check if required fields are listed
+  //const requiredFields = ['firstName', 'lastName', 'username', 'password', 'email'];
+  // for(let keys in req.body){
+  //   requiredFields.forEach(reqKey => {
+  //   if(keys === reqKey) {
+  //     console.log(true);
+  //   }
+  //   else {
+  //     console.log(false);
+  //   }
+  // })
+
+  User.create({
+    firstName: req.body.firsName,
+    lastName: req.body.lastName,
     username: req.body.username,
     password: req.body.password,
     email: req.body.email
-  });
+  })
+  .then(user =>
+    res.json({username: user.username})
+  )
 };
 
 const updateUser = (req, res) => {
-
+  User.findByIdAndUpdate(req.params.id)
+  .then()
 };
 
 const deleteUser = (req, res) => {
-
+  //to delete user we need to get the username and password
+  User.deleteOne({ username: req.body.username, password: req.body.password })
+  .then(res.json(`The account named ${req.body.username} has been deleted.`));
 };
 
-module.exports = { getAllUsers, getUserById, createUser, updateUser, deleteUser };
+module.exports = { getAllUsers, getUser, createUser, updateUser, deleteUser };
--- api/users/userRouter.js
+++ api/users/userRouter.js
@@ -6,23 +6,24 @@ const userController = require('./userController');
 const userRouter = express.Router();
 
 //route to get a list of all users
-userRouter.route('/users')
-.get((req, res) => {
+userRouter.get('/users', (req, res) => {
   userController.getAllUsers(req, res);
 })
-.post((req, res) => {
+
+userRouter.post('/user/createUser', (req, res) => {
   userController.createUser(req, res);
 });
 
 //main route for requests in regards to users by id
 userRouter.route('/user/:id')
 .get((req,res) => {
-  userController.getUserById(req, res);
+  userController.getUser(req, res);
 })
 .put((req, res) => {
   userController.updateUser(req, res);
-})
-.delete((req, res) => {
+});
+
+userRouter.delete('/user/deleteAccount', (req, res) => {
   userController.deleteUser(req, res);
 });
 
