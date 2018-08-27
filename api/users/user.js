3f6e48daae819b0d90292f9bad529c02f0b6cea9
unknown <aaron.leyesa@gmail.com>
2018-08-26 18:23:15 -0700

    added base files

--- api/users/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRouter = require('./userRouter');
-
-const app = express();
-
-app.use(userRouter);
-
-module.exports = app;
--- server.js
+++ server.js
@@ -2,15 +2,9 @@
 const express = require('express');
 //import bodyParser middleware
 const bodyParser = require('body-parser');
-const user = require('./api/users/user');
-//create an express application
+
 const app = express();
 //use applications
-
-//use bodyParser middleware to parse json string from body.
-app.use(bodyParser.json());
-//use users http request methods from userRouter file.
-app.use('/api', user);
 
 //run local host
 app.listen(8080, () => console.log('Application listening to port 8080'));
